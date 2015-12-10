53ce2258724267ec6f64f7025575de4adcb6ec63
TJ VanToll <tj.vantoll@gmail.com>
2015-12-10 10:19:55 -0500

    Let’s try fixing this service worker again

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,26 @@
 var version = "2.0::";
 var offlineResources = [
-	"/css/kendo.common-material.min.css",
-	"/css/kendo.material.min.css",
-	"/font-awesome/css/font-awesome.min.css",
-	"/font-awesome/fonts/fontawesome-webfont.woff2",
-	"/css/app.css",
-	"/js/jquery.min.js",
-	"/js/kendo.custom.min.js",
-	"/js/app.js"
+	"/",
+	"css/kendo.common-material.min.css",
+	"css/kendo.material.min.css",
+	"font-awesome/css/font-awesome.min.css",
+	"font-awesome/fonts/fontawesome-webfont.woff2",
+	"css/app.css",
+	"js/jquery.min.js",
+	"js/kendo.custom.min.js",
+	"js/app.js"
 ];
 self.addEventListener("install", function(event) {
 	event.waitUntil(
 		caches
 			.open(version + "static")
 			.then(function(cache) {
-				console.log("1");
 				cache.addAll(offlineResources);
-				console.log("2");
 			})
 	);
 });
 
 self.addEventListener("activate", function(event) {
-	console.log("3");
 	event.waitUntil(
 		caches.keys().then(function(keys) {
 			return Promise.all(keys
@@ -35,5 +33,17 @@ self.addEventListener("activate", function(event) {
 			);
 		})
 	);
-	console.log("4");
+});
+
+self.addEventListener("fetch", function(event) {
+	var request = event.request;
+	event.respondWith(
+		caches.match(request)
+			.then(function(response) {
+				return response || fetch(request);
+			})
+			.catch(function() {
+				return fetch(request);
+			})
+	);
 });
